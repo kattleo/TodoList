3ee8a527fe6f963afc76186733f24896572732a6
Leo Kattenbeck <kattleo584@gmail.com>
2024-05-25 23:11:36 +0200

    Added HTML, CSS and Frontend-Logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+class Task {
+  public taskText: string;
+  public isComplete: boolean;
+
+  constructor(taskText: string, isComplete: boolean) {
+    this.taskText = taskText;
+    this.isComplete = isComplete;
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TodoList';
+  @Input() newTaskText: string = '';
+
+  tasks: Task[] = [
+    new Task('Get 100,000$', false),
+    new Task('Eat Breakfast', true)
+  ]
+
+  toggleTask(task: Task) {
+    task.isComplete = !task.isComplete;
+  }
+
+  addNewTask() {
+    this.tasks.push(new Task(this.newTaskText, false));
+  }
 }
