a082b4ade474bb430f4293768e2e02578fac65a8
Leo Kattenbeck <kattleo584@gmail.com>
2024-05-29 01:05:49 +0200

    Added Backend Source Files

---
+++ server.py
@@ -0,0 +1,95 @@
+from flask import Flask, jsonify
+from flask_restful import reqparse
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from flask_cors import CORS
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+# create the Flask app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+# handle CORS (for simplicity, every origin is allowed)
+CORS(app)
+db.init_app(app)
+
+
+class Task(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    taskText: Mapped[str] = mapped_column(nullable=False)
+    isComplete: Mapped[bool] = mapped_column(nullable=False, default=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'taskText': self.taskText,
+            'isComplete': self.isComplete
+        }
+
+
+# execute only once to initially initialize DB with its Model
+# with app.app_context():
+#     db.create_all()
+
+
+# Create Request Parsers
+request_parser = reqparse.RequestParser()
+request_parser.add_argument("taskText", type=str, help="Task Text")
+request_parser.add_argument("id", type=str, help="Task ID")
+
+
+@app.route("/tasks")
+def get_tasks():
+    try:
+        tasks = Task.query.all()
+        return jsonify([task.to_dict() for task in tasks]), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/tasks/create", methods=["POST"])
+def create_task():
+    try:
+        args = request_parser.parse_args()
+        new_task = Task(taskText=args['taskText'])
+        db.session.add(new_task)
+        db.session.commit()
+        return new_task.to_dict(), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/tasks/remove", methods=["DELETE"])
+def remove_task():
+    try:
+        args = request_parser.parse_args()
+        task_id = args['id']
+        task = db.session.execute(db.select(Task).filter_by(id=task_id)).scalar_one()
+        db.session.delete(task)
+        db.session.commit()
+        return jsonify({"Removed Task": task.to_dict()}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/tasks/update", methods=["POST"])
+def update_task():
+    try:
+        args = request_parser.parse_args()
+        task_id = args['id']
+        task = db.session.execute(db.select(Task).filter_by(id=task_id)).scalar_one()
+        task.isComplete = not task.isComplete
+        db.session.commit()
+        return jsonify({"Updated Task": task.to_dict()}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
